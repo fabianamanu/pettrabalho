8c82728acc512bb9a67c56f1ef87548a5d2b5b35
info206 <info206@LAB02-10.edupit.intra>
2015-06-01 14:44:22 -0300

    1234567

---
+++ src/pettrabalho/Pettrabalho.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pettrabalho;
+
+/**
+ *
+ * @author info206
+ */
+public class Pettrabalho {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
