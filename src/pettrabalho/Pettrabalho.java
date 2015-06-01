0ea6ba199bbe995d585b103b7aba3537b4c07950
info206 <info206@LAB02-10.edupit.intra>
2015-06-01 14:51:39 -0300

    novo

--- src/pettrabalho/Pettrabalho.java
+++ src/pettrabalho/Pettrabalho.java
@@ -14,6 +14,7 @@ public class Pettrabalho {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("kkk");
         // TODO code application logic here
     }
 }
